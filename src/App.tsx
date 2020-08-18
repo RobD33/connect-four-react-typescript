1a2f57b8f2dae2a893be83a0e00104b92a09e187
Rob Davidson <r.a.davidson@live.co.uk>
2020-08-18 16:49:08 +0100

    create changePlayer callback in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import Board from './components/Board/Board'
 
 function App() {
 
+  const [state, setState] = useState({ player: 1 })
+
+  const changePlayer = useCallback(() => {
+    let newState = state.player === 1 ? { player: 2 } : { player: 1 }
+    setState(newState)
+  }, [state])
+  
   return (
     <div>
       <Board />
